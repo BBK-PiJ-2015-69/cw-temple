913057d80017c2ff62119401ae86857d4b4f14e0
Luke Jones <luke.jones2@directwine.com>
2016-04-07 18:46:22 +0100

    updated ROOT from /Users/keith/Courses/PiJ/repo/cw-temple/res/ to local machine address

--- src/gui/ROOT.java
+++ src/gui/ROOT.java
@@ -1,5 +1,5 @@
 package gui;
 
 interface ROOT {
-    final static String ROOT = "/Users/keith/Courses/PiJ/repo/cw-temple/res/";
+    final static String ROOT = "/Users/lukejones/Documents/Birkbeck/PiJ/cw-temple/res/";
 }
