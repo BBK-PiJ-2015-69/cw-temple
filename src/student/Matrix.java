10af1fbbada7efe1b0dc9b5dc72b0c879d6930cc
Luke Jones <luke.jones2@directwine.com>
2016-04-18 22:52:23 +0100

    Matrix.java needs the getNeighbours class

--- src/student/Matrix.java
+++ src/student/Matrix.java
@@ -80,49 +80,11 @@ public class Matrix{
         return gold.containsKey(node);
     }
 
- 	public List<Long> getEvaluationNode(long id, boolean neighboursExist){
+    public Set<Long> getNeighbours(long currentLocation){
         
-        //store here the nodes
-        Set<Long> nodes;
-
-        //if now neighbouring nodes
-        if(!neighboursExist){
-            //add all unvisited nodes
-            nodes = unvisited.keySet();
-        }else{
-            //add neighbouring nodes
-            nodes = getNeighbours(id);
-        }
+        Set<Long> neighboursData = (Set<Long>) this.unvisited.keySet();
         
-        //store here possible needs to visit
-        List<MatrixNode> availableNodes = new ArrayList(); 
-
-        //scan nodes
-        for(long n : nodes){
-
-            //if unvisited
-            if(!visited.containsKey(n)){
-
-                //get distance
-                int distance = (Integer) targets.get(n);
-                //add to availableNodes
-                availableNodes.add(new MatrixNode(n, distance));
-
-            }
-
-        }
-
-        //store here nodes to evaluate
-        List<Long> evaluationNodes = new ArrayList();
-        //sort the avialable unvisited nodes
-        Collections.sort(availableNodes);
-        
-        //scan available nodes and add to evaluationNodes, now sorted in order
-        for (MatrixNode available : availableNodes){
-            evaluationNodes.add(available.getNode());
-        }
-        
-        return evaluationNodes;
+        return neighboursData;
 
     }
 
@@ -196,6 +158,52 @@ public class Matrix{
         }while(!edges.isEmpty());
 
         return dijkstrasPath;
+
+    }
+
+ 	public List<Long> getEvaluationNode(long id, boolean neighboursExist){
+        
+        //store here the nodes
+        Set<Long> nodes;
+
+        //if now neighbouring nodes
+        if(!neighboursExist){
+            //add all unvisited nodes
+            nodes = unvisited.keySet();
+        }else{
+            //add neighbouring nodes
+            nodes = getNeighbours(id);
+        }
+        
+        //store here possible needs to visit
+        List<MatrixNode> availableNodes = new ArrayList(); 
+
+        //scan nodes
+        for(long n : nodes){
+
+            //if unvisited
+            if(!visited.containsKey(n)){
+
+                //get distance
+                int distance = (Integer) targets.get(n);
+                //add to availableNodes
+                availableNodes.add(new MatrixNode(n, distance));
+
+            }
+
+        }
+
+        //store here nodes to evaluate
+        List<Long> evaluationNodes = new ArrayList();
+        //sort the avialable unvisited nodes
+        Collections.sort(availableNodes);
+        
+        //scan available nodes and add to evaluationNodes, now sorted in order
+        for (MatrixNode available : availableNodes){
+            evaluationNodes.add(available.getNode());
+        }
+        
+        return evaluationNodes;
 
     } 
 
