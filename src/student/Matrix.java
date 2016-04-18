e4d014f4d9ef98fd934f7cad673193c2f7169df3
Luke Jones <luke.jones2@directwine.com>
2016-04-18 19:58:36 +0100

    creating matrix constructor

--- src/student/Matrix.java
+++ src/student/Matrix.java
@@ -4,5 +4,15 @@ import java.lang.IllegalArgumentException;
 
 public class Matrix{
 
+    private Map unvisited;
+    private Map visited;
+
+    public Matrix(){
+
+        unvisited = new HashMap(999);
+        visited = new HashMap(999);
+
+    }
+
 
 }
