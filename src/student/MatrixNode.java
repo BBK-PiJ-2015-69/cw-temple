0f250beb331f02b69bc1703f65a4c8628de0055b
Luke Jones <luke.jones2@directwine.com>
2016-04-18 21:38:18 +0100

    oops MatrixNode has no constructor

--- src/student/MatrixNode.java
+++ src/student/MatrixNode.java
@@ -4,5 +4,10 @@ public class MatrixNode implements Comparable<MatrixNode> {
 
     long node;
     Integer distance;
+    
+    public MatrixNode(long node, int distance){
+        this.node = node;
+        this.distance = distance;
+    } 
 
 }
